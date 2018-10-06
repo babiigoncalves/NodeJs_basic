4e9159d864561c41fb5feb285721c07b45df8590
Barbara <babiigoncalves@gmail.com>
2018-10-06 16:24:35 -0300

    Adicionando evento

--- helloWorld/helloNode.js
+++ helloWorld/helloNode.js
@@ -1,7 +1,12 @@
 var http = require('http');
-http.createServer(function(req, res){
+
+var server = http.createServer();
+
+server.on('request', function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.end('<h1> Hello World! </h1>');
-}).listen(3000);
+});
+
+server.listen(3000);
 
 console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar');
