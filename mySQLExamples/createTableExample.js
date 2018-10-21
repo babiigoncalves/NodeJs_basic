2c6f8c41710154590cb3c06fe346927f3f22fee1
Barbara <babiigoncalves@gmail.com>
2018-10-21 16:08:49 -0300

    Criação de tabela com primary key

--- mySQLExamples/createTableExample.js
+++ mySQLExamples/createTableExample.js
@@ -10,7 +10,7 @@ var conn = mysql.createConnection({
 conn.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
   conn.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
